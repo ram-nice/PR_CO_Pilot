e1428419f91b2fd30ad5d6006dcdcb380477c7c0
Ramprasadh Natarajan <Ramprasadh.Natarajan@nice.com>
2024-03-03 11:46:07 +0530

    optimises the functionality

--- professor.js
+++ professor.js
@@ -4,111 +4,72 @@ const cssConfig = require('./KeywordsAndErrorConfigs/cssConfig.json');
 const tsxConfig = require('./KeywordsAndErrorConfigs/tsxConfig.json');
 const commonConfig = require('./KeywordsAndErrorConfigs/commonConfig.json');
 
-let corrections = new Set();
 
-const addLog = (checkType, index, fileType)=>{
-    if(fileType === 'css'){
-        corrections.add(`at line number ${index}, ${cssConfig[checkType]}`);
-    }
-    else if(fileType === 'tsx'){
-        corrections.add(`at line number ${index}, ${tsxConfig[checkType]}`);
-    }
-    else if(fileType === 'common'){
-        corrections.add(`at line number ${index}, ${commonConfig[checkType]}`);
-    }
-};
 
-const checkInLine = (keyword, line, index, checkType)=>{
-    line.match(new RegExp(keyword)) && addLog(keyword,index+1,checkType);
-}
-function findMistakesInStylesFile(fileContent) {
-    // Parse CSS files
-        const lines = fileContent.split('\n');
-        lines.map((line, index) =>{
-            line=line.trim();
-            if(line.match(/\$/)){
-                const subLine = line.substring(line.indexOf('{'), line.indexOf('}'));
-                const operatorIndex = [subLine.indexOf('?'),subLine.indexOf('.'),subLine.lastIndexOf('?'),subLine.lastIndexOf('.')];
-                if((operatorIndex[0] !== (operatorIndex[1]-1))||(operatorIndex[2] !== (operatorIndex[3]-1))){
-                    addLog('?',index+1, 'common');
-                }
-            };
-            checkInLine('px',line, index,'css');
-            checkInLine('!important',line, index, 'css');
-            checkInLine('#',line, index, 'css');
-        });
+const loadConfigs = (fileExtension)=>{
+    // let keywords = [...commonConfig?.keywords];
+    // let errors =  commonConfig?.errors;
 
-    // Generate report
-    let report = {
-        corrections: [...corrections],
-    };
-
-    return report;
-}
-
-const findMistakesInComponentFile=(File)=> {
-    // Parse Component files
-        const content = fs.readFileSync(File, 'utf-8');
-        const lines = content.split('\n');
-
-        lines.map((line, index) =>{
-            checkInLine('any', line, index, 'tsx');
-            checkInLine('"', line, index, 'tsx');
-            checkInLine('console', line, index,'tsx');
-        });
-
-    // Generate report
-    let report = {
-        corrections: [...corrections],
-    };
-
-    return report;
-}
-const displayResults=(corrections)=> {
-    if(corrections?.length > 0){
-        console.error("Found below mistakes :", corrections);
+    let keywords=[];
+    let errors={};
+    if(fileExtension === '.tsx'){
+        keywords =  [...keywords, ...tsxConfig?.keywords];
+        errors = {...errors, ...tsxConfig?.errors};
     }
     else{
-        console.log("Great! the file looks fine ..!");
+        keywords =  [...keywords, ...cssConfig?.keywords];
+        errors = {...errors, ...cssConfig?.errors};
     }
-console.log('check completed');
-console.log('-------------------------------------------------------------------------------')
+    return {keywords, errors};
 }
 
+const findMistakesInFile = (file, keywords, errors)=>{
+    const content = fs.readFileSync(file, 'utf-8');
+    const lines = content.split('\n');
+    let corrections = [];
+    lines.map((line, index) =>{
+        line = line.trim();
+        if(line){
+            keywords.map((keyword)=>{   
+                line.indexOf(keyword) !== -1 && corrections.push(`at line number ${index+1}, ${errors[keyword]}`);
+            });
+        }
+         return corrections;
+    });
+
+    return corrections;
+
+}
 
 let args = process.argv.slice(2);
-const content = fs.readFileSync(args[1], 'utf-8');
 console.log('-------------------------------------------------------------------------------');
 console.log('Initialising...');
 
 // Check if there are enough arguments
-if (args.length < 2  ) {
+if (args.length < 1  ) {
     console.error('Usage: node script.js <file1> <file2>');
     process.exit(1);
 }
-
-args = args.slice(1);
-
 args.map((file)=>{
     const fileName = file.split('\\')[file.split('\\').length - 1];
     const fileExt = fileName.slice(fileName.indexOf('.'));
-   // loadConfigs(fileName,fileExt);
-    if (file.endsWith('styles.ts')){
-        console.log(`checking the style file ${fileName} ${fileExt}...`);
-        const result = findMistakesInStylesFile(content);
-        displayResults(result.corrections);
-    }
-    else if(file.endsWith('spec.ts')){
-        console.log(`checking the tests in file ${file} ...`);
-    }
-    else if(file.endsWith('.tsx')){
-        console.log(`checking the Component file ${file} ${fileExt}...`);
-        const result = findMistakesInComponentFile(file);
-        displayResults(result.corrections);
+    const {keywords, errors} = loadConfigs(fileExt);
+    if(fileExt === '.tsx' || fileExt === '.styles.ts'|| fileExt === '.style.ts' || fileExt === '.spec.ts'){
+        console.log(`checking the ${fileExt} file ${fileName}...`);
+        const corrections = findMistakesInFile(file, keywords, errors);
+        if(corrections?.length > 0){
+            console.log(`Found ${corrections?.length} mistakes`)
+            console.error("list of mistakes :", corrections);
+        }
+        else{
+            console.log("Great! the file looks fine ..!");
+        }
+    console.log('check completed');
+    console.log('-------------------------------------------------------------------------------')
     }
     else{
         console.log(`checking the file ${fileName} ${fileExt}...`);
-        console.error('Apologies !, Currently we dont support this extension');
+        console.error(`Apologies !, Currently we dont support this extension ${fileExt}`);
         process.exit(1);
     }
 })
